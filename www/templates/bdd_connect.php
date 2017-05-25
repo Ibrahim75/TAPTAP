b6e9b6bf4941a8d6a4465227d3b98e4c7451cd6f
Ibrahim75 <ibra_lion_du75013@hotmail.fr>
2017-05-25 18:43:17 +0200

    bbd connect
    bdd conesct

--- www/templates/bdd_connect.php
+++ www/templates/bdd_connect.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $bdd = new PDO('mysql:host=localhost:8889;dbname=teechme;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost:3306;dbname=teechme;charset=utf8', 'root', 'NON');
 
 }
 catch(Exception $e)
